a9dc4aae7976e762816069910270c5b9f2fbd83d
Tobe Okafor <teokafor@gmail.com>
2025-04-19 22:31:30 -0500

    Add project styles for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 export function App() {
   return (
     <>
-      <div className='header-box'>
+      <div className="header-box">
         <img className="headshot"></img>
         <div>
           <h1 className="test size">Tobechukwu Okafor</h1>
@@ -17,7 +17,7 @@ export function App() {
       </div>
 
       <Section head={"About Me"}>
-        <div className='section-flex'>
+        <div className="section-flex odd">
           <div className="body-text">
             <p>
               <br />
@@ -40,48 +40,57 @@ export function App() {
 
       <Section head={"Projects"}>
         <Section subhead={"Project Name I"}>
-          <div className="body-text">
-            <p>
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              sagittis lorem in lorem eleifend commodo. Vivamus ullamcorper.
-              <br />
-              <br />
-              Convallis urna vel varius. Proin mattis, quam at consequat
-              tincidunt, orci dolor rhoncus dui, vel cursus tellus eros posuere
-              risus. Proin et euismod leo. Etiam blandit dolor mauris, sit amet
-              dictum ipsum consequat vel.
-              <br />
-              <br />
-              Built with <span>tool1,</span> <span>tool2,</span> and{" "}
-              <span>tool3.</span>
-            </p>
-          </div>
-          <Carousel />
+          <div className="section-flex even">
+            <div className='project-flex'>
+            <div className="body-text">
+              <p>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                sagittis lorem in lorem eleifend commodo. Vivamus ullamcorper.
+                <br />
+                <br />
+                Convallis urna vel varius. Proin mattis, quam at consequat
+                tincidunt, orci dolor rhoncus dui, vel cursus tellus eros
+                posuere risus. Proin et euismod leo. Etiam blandit dolor mauris,
+                sit amet dictum ipsum consequat vel.
+                <br />
+                <br />
+                Built with <span>tool1,</span> <span>tool2,</span> and{" "}
+                <span>tool3.</span>
+              </p>
+            </div>
+            <Carousel />
+            </div>
           <Particles />
+          </div>
         </Section>
         <Section subhead={"Project Name II"}>
-          <div className="body-text">
-            <p>
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              sagittis lorem in lorem eleifend commodo. Vivamus ullamcorper.
-              <br />
-              <br />
-              Convallis urna vel varius. Proin mattis, quam at consequat
-              tincidunt, orci dolor rhoncus dui, vel cursus tellus eros posuere
-              risus. Proin et euismod leo. Etiam blandit dolor mauris, sit amet
-              dictum ipsum consequat vel.
-              <br />
-              <br />
-              Built with <span>tool1,</span> <span>tool2,</span> and{" "}
-              <span>tool3.</span>
-            </p>
+          <div className="section-flex odd">
+            <div className="project-flex">
+              <div className="body-text">
+                <p>
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  sagittis lorem in lorem eleifend commodo. Vivamus ullamcorper.
+                  <br />
+                  <br />
+                  Convallis urna vel varius. Proin mattis, quam at consequat
+                  tincidunt, orci dolor rhoncus dui, vel cursus tellus eros posuere
+                  risus. Proin et euismod leo. Etiam blandit dolor mauris, sit amet
+                  dictum ipsum consequat vel.
+                  <br />
+                  <br />
+                  Built with <span>tool1,</span> <span>tool2,</span> and{" "}
+                  <span>tool3.</span>
+                </p>
+              </div>
+              <Carousel />
+            </div>
+              <Particles />
           </div>
-          <Carousel />
-          <Particles />
         </Section>
         <Section subhead={"Project Name III"}>
+          <div className="section-flex even">
           <div className="body-text">
             <p>
               <br />
@@ -100,11 +109,12 @@ export function App() {
             </p>
           </div>
           <Particles />
+          </div>
         </Section>
       </Section>
 
       <Section head={"Contact"} subhead={" "}>
-        <p className='footer-text'>
+        <p className="footer-text">
           Website made with ♥️ (& <span>React</span> + <span>Tailwind</span> +{" "}
           <span>Pts.js</span>.)
         </p>
